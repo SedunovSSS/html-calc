b69b92bfd30563eda05f28c690a1f49a0773782f
SedunovSSS <sedunov308@gmail.com>
2023-03-05 08:23:19 +0500

    calc

---
+++ index.js
@@ -0,0 +1,44 @@
+function f(text){
+    if (document.getElementById("c").innerHTML == "0"){
+        document.getElementById("c").innerHTML = "";
+    }
+    document.getElementById("c").innerHTML += text;
+}
+function t(text){
+    document.getElementById("c").innerHTML += text;
+}
+function ac(){
+    document.getElementById("c").innerHTML = "0";
+}
+function del(){
+    document.getElementById("c").innerHTML = document.getElementById("c").innerHTML.slice(0, -1);
+    if (document.getElementById("c").innerHTML == ""){
+        document.getElementById("c").innerHTML = "0";
+    }
+}
+function sOpen(){
+    if (document.getElementById("c").innerHTML == "0"){
+        document.getElementById("c").innerHTML = "";
+    }
+    document.getElementById("c").innerHTML += '(';
+}
+
+function sClose(){
+    if (document.getElementById("c").innerHTML == "0"){
+        document.getElementById("c").innerHTML = "";
+    }
+    document.getElementById("c").innerHTML += ')';
+}
+
+function calculate(){
+    try{
+        document.getElementById("c").innerHTML = eval(document.getElementById("c").innerHTML);
+        if (document.getElementById("c").innerHTML == "Infinity" | document.getElementById("c").innerHTML == "NaN"){
+            document.getElementById("c").innerHTML = "0";
+        }
+    }
+    catch(error){
+        console.log(error);
+        document.getElementById("c").innerHTML = "0";
+    }
+}
